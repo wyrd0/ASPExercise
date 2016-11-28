2642f15d3ef74730575ad4f12f58dc9b1d07e957
Mary Olive <mary.olive@me.com>
2016-11-28 09:39:03 -0500

    in class work 11/23

---
+++ CoffeShopApp/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoffeShopApp.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProcessSignup(string UName, string Email)
+        {
+            ViewBag.Message = "Thanks, " + UName + ".\n We're sending an email to "+ Email+".";
+                return View("Index");
+            //return Redirect("https://www.google.com");
+        }
+    }
+}
